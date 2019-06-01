5011a17af32e7c8d37fb9028bbd9fc7d61e6e2dc
Alex Siler <davidalexsiler@gmail.com>
2019-06-01 03:59:40 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -18,7 +18,7 @@ def first_challenge
     data.each do |attribute, value|
        if attribute == :favorite_icecream_flavors
       value.collect do |flavor|
-        if flavor == "strawberry"
+        
           
           
         
