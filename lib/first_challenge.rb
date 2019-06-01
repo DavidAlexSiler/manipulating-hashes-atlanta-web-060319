6909d621113f4976b55d7b170cfa0a3018085b6b
Alex Siler <davidalexsiler@gmail.com>
2019-06-01 04:10:03 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -17,11 +17,7 @@ def first_challenge
   contacts.each do |person, data|
     data.each do |attribute, value|
        if attribute == :favorite_icecream_flavors
-      value.each do |flavor|
-        flavors << flavor
-      end
-        flavors.delete_if do |flave|
-          flave == "strawberry"
+         
           
           
         
